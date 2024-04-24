737ff016bc074ab0335565be871c7ade779f4b00
Lambda0x0 <luciano.lamberti00@gmail.com>
2024-04-24 12:43:09 +0200

    aggiunto AdminDao Admin nel model

---
+++ friends/src/main/java/com/example/friends/model/Admin.java
@@ -0,0 +1,60 @@
+package com.example.friends.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    @NotNull
+    private String username;
+
+    @Column(nullable = false, length = 40)
+    @NotNull
+    private String password;
+
+    // Costruttori, getter e setter
+
+    public Admin() {
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter e setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
