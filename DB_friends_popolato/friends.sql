1931c3546adc0a5f55c2b752d34536bc24e6fb51
fabianaFar <fariasfabiana922@gmail.com>
2024-04-24 12:12:49 +0200

    dati popolati

--- DB_friends_popolato/friends.sql
+++ DB_friends_popolato/friends.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 24, 2024 alle 11:17
+-- Creato il: Apr 24, 2024 alle 12:12
 -- Versione del server: 10.4.27-MariaDB
 -- Versione PHP: 7.4.33
 
@@ -21,122 +21,18 @@ SET time_zone = "+00:00";
 -- Database: `friends`
 --
 
--- --------------------------------------------------------
-
 --
--- Struttura della tabella `admin`
+-- Dump dei dati per la tabella `categorie`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `categorie`
---
-
-CREATE TABLE `categorie` (
-  `id_categoria` int(11) NOT NULL,
-  `nome_categoria` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `contenuti`
---
-
-CREATE TABLE `contenuti` (
-  `id_contenuto` int(11) NOT NULL,
-  `titolo` varchar(60) NOT NULL,
-  `descrizione` text NOT NULL,
-  `fk_id_galleria` int(11) NOT NULL,
-  `fk_id_categoria` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `galleria`
---
-
-CREATE TABLE `galleria` (
-  `id_galleria` int(11) NOT NULL,
-  `foto` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indici per le tabelle `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id_categoria`);
-
---
--- Indici per le tabelle `contenuti`
---
-ALTER TABLE `contenuti`
-  ADD PRIMARY KEY (`id_contenuto`),
-  ADD KEY `contenuti` (`fk_id_categoria`),
-  ADD KEY `contenuto` (`fk_id_galleria`);
-
---
--- Indici per le tabelle `galleria`
---
-ALTER TABLE `galleria`
-  ADD PRIMARY KEY (`id_galleria`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `contenuti`
---
-ALTER TABLE `contenuti`
-  MODIFY `id_contenuto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `galleria`
---
-ALTER TABLE `galleria`
-  MODIFY `id_galleria` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `contenuti`
---
-ALTER TABLE `contenuti`
-  ADD CONSTRAINT `contenuti` FOREIGN KEY (`fk_id_categoria`) REFERENCES `categorie` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `contenuto` FOREIGN KEY (`fk_id_galleria`) REFERENCES `galleria` (`id_galleria`) ON DELETE CASCADE ON UPDATE CASCADE;
+INSERT INTO `categorie` (`id_categoria`, `nome_categoria`) VALUES
+(1, 'La storia di Friends'),
+(2, 'Personaggi'),
+(3, 'Location'),
+(4, 'Scene iconiche'),
+(5, 'News'),
+(6, 'Gadget'),
+(7, 'Backstage');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
