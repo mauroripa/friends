b13c82d5ce717c0e16f110abb62170e9192b2b3c
Lambda0x0 <luciano.lamberti00@gmail.com>
2024-04-26 11:53:44 +0200

    aggiunta logica di tentativi accessi fixato

--- friends/src/main/java/com/example/friends/controller/LoginAdminController.java
+++ friends/src/main/java/com/example/friends/controller/LoginAdminController.java
@@ -2,12 +2,13 @@ package com.example.friends.controller;
 
 import com.example.friends.service.AdminService;
 import com.example.friends.service.ServizioTentativiAccesso;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class LoginAdminController {
@@ -25,17 +26,17 @@ public class LoginAdminController {
 
     @PostMapping("/admin/login")
     public String loginAdmin(String username, String password, Model model, HttpSession session) {
-        if (servizioTentativiAccesso.isBloccato(username)) {
+        if (servizioTentativiAccesso.isBloccato(session)) {
             model.addAttribute("error", "Utente bloccato. Riprova più tardi.");
             return "loginAdmin";
         }
 
         boolean isLoggedIn = adminService.loggaAdmin(username, password, session);
         if (isLoggedIn) {
-            servizioTentativiAccesso.accessoRiuscito(username);
+            servizioTentativiAccesso.accessoRiuscito(session);
             return "redirect:/areariservata";
         } else {
-            servizioTentativiAccesso.accessoFallito(username);
+            servizioTentativiAccesso.accessoFallito(session);
             model.addAttribute("error", "Credenziali non valide. Riprova.");
             return "loginAdmin";
         }
