32c92d5a7df15499c73fd2879b7e6bdc11960fb8
Mauro Ripa <mauroripa7@gmail.com>
2024-04-26 10:23:37 +0200

    alcuni miglioramenti grafici
    miglioramenti grafici ad index

---
+++ VIEW - Friends/index.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    let dropdown = document.getElementById("dropdown-content");
+    if (dropdown.classList.contains("dropdown-hidden")) {
+      dropdown.classList.remove("dropdown-hidden");
+    } else {
+      dropdown.classList.add("dropdown-hidden");
+    }
+  }
