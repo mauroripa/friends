c236d3cff80323a7c96519dfac120575ed8222a7
ChiaraDiCarlo <chiaradicarlo26@gmail.com>
2024-04-29 17:12:32 +0200

    Aggiunta th categorie in index

--- friends/src/main/java/com/example/friends/controller/IndexController.java
+++ friends/src/main/java/com/example/friends/controller/IndexController.java
@@ -1,14 +1,25 @@
 package com.example.friends.controller;
+import com.example.friends.model.Categoria;
+import com.example.friends.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     @GetMapping
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        List<Categoria> categorie = categoriaService.listaCategorie();
+        model.addAttribute("categorie", categorie);
         return "index";
     }
 }
