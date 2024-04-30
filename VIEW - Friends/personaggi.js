d148e3c810f3e9d222f3801e9d18fa8945c8fb10
Mauro Ripa <mauroripa7@gmail.com>
2024-04-30 11:42:33 +0200

    Aggiunta buttons Personaggi
    Aggiunta button personaggi per tornare su

--- VIEW - Friends/personaggi.js
+++ VIEW - Friends/personaggi.js
@@ -5,3 +5,7 @@ function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId); 
     section.scrollIntoView({ behavior: 'smooth' }); 
   } 
+function scrollArrowUp(id){
+  var sectionTop = document.getElementById(id);
+  sectionTop.scrollIntoView({ behavior:'smooth'});
+}
