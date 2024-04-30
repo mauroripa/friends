bf086401b1dbe80cca6854fcced12c87c75e0752
fabianaFar <fariasfabiana922@gmail.com>
2024-04-30 12:03:05 +0200

    push a caso

--- friends/src/main/java/com/example/friends/controller/ContenutoController.java
+++ friends/src/main/java/com/example/friends/controller/ContenutoController.java
@@ -1,7 +1,6 @@
 package com.example.friends.controller;
 
 import com.example.friends.model.Categoria;
-import com.example.friends.model.Contenuto;
 import com.example.friends.service.CategoriaService;
 import com.example.friends.service.ContenutoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping ("/{path}")
