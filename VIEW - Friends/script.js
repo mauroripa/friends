18135b1b1fc5870587406fd87967a61e984ba3e4
GianmarcoA. <gianmarcoamendola00@gmail.com>
2024-04-30 16:33:21 +0200

    modifiche varie

--- VIEW - Friends/script.js
+++ VIEW - Friends/script.js
@@ -1,54 +1,62 @@
 const card = document.querySelectorAll('.card');
 
-/*
-const image = card.querySelector('img'); 
-const video = card.querySelector('video1');
+card.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    const image = item.querySelector('img');
+    const video = item.querySelector('video');
+    const testo = item.querySelector('.titolocard');
+    const paragrafo = item.querySelector('.paragrafo');
+  
+    
 
+    if (video) {
+      // Nascondere l'immagine
+      
+      image.style.display = 'none';
+      paragrafo.style.display = 'block';
+      testo.style.display = 'block';
+      video.style.display = 'block';
+      video.style.opacity = 0;
+      video.play();
 
-*/
-card.forEach(item => item.addEventListener('mouseover', () => {
+      // Dissolvenza solo sul video
+      setTimeout(() => {
+        video.style.opacity = 1;
+      }, 100);
 
+      // Aggiungi sfondo scuro
+      document.body.classList.remove('bg-body-tertiary');
+      document.getElementById('testo').classList.add('bg-body-tertiary');
+      document.getElementById('testo').style.opacity='5%';
+      document.getElementById('divlocation').style.opacity='50%';
+      document.getElementById('divlocation').style.zIndex='-1';
 
-  const image = item.querySelector('img');
-  const video = item.querySelector('video');
-  if (video) {
-    console.log(image);
-    console.log(video);
-    // Nascondere l'immagine
+    }
+  });
 
-    image.style.display = 'none';
+  item.addEventListener('mouseleave', () => {
+    const image = item.querySelector('img');
+    const video = item.querySelector('video');
+    const testo = item.querySelector('.titolocard');
+    const paragrafo = item.querySelector('.paragrafo');
 
-    video.style.display = 'block';
-
-    video.style.opacity = 0;
-
-    video.play();
-
-    // Dissolvenza solo sul video
-    setTimeout(() => {
-      video.style.opacity = 1;
-    }, 100);
-  }
-
-}));
-
-
-card.forEach(item => item.addEventListener('mouseout', () => {
-  const image = item.querySelector('img');
-  const video = item.querySelector('video');
-
-  if (video) {
-
-    video.style.display = 'none';
-
-    // Transizione video
-    video.style.transition = "opacity 1s";
-
-    video.style.display = 'none';
-    image.style.display = 'block';
-  }
-
-}));
+    if (video) {
+      video.style.display = 'none';
+      testo.style.display = 'none';
+      paragrafo.style.display = 'none';
+      // Transizione video
+      video.style.transition = "opacity 1s";
+      video.style.display = 'none';
+      image.style.display = 'block';
+      
+      // Rimuovi sfondo scuro
+      document.body.classList.add('bg-body-tertiary');
+      document.getElementById('testo').classList.remove('bg-body-tertiary');
+      document.getElementById('testo').style.opacity='100%';
+      document.getElementById('divlocation').style.opacity='100%';
+    }
+  });
+});
 
 
 
