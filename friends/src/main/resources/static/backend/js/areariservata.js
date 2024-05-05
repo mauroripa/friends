ef0ff8d8717875c1605dfc61beb48898fc446285
Andrei Leca <HMDCrew@users.noreply.github.com>
2024-05-05 14:59:47 +0200

    thumbnails preview before upload images

--- friends/src/main/resources/static/backend/js/areariservata.js
+++ friends/src/main/resources/static/backend/js/areariservata.js
@@ -22,22 +22,27 @@ new_category_btn.addEventListener('click', ev=>hundle_click(), false)
 
 */
 
+const column_fix_height = (container) => {
 
+    const carta = container.querySelector('.carta')
+    const fix_content_height = carta.querySelector('.carta-content-fix')
+
+    carta.style.height = (
+        container.classList.contains('open')
+        ? (fix_content_height.offsetHeight + 10) + 'px'
+        : 0
+    )
+}
 
 const handle_column = (btn) => {
 
     const container = btn.closest('.col-content-manager')
     const carta = container.querySelector('.carta')
-    const fix_content_height = carta.querySelector('.carta-content-fix')
 
     container.classList.toggle('open')
     carta.scrollTop = 0;
 
-    carta.style.height = (
-        container.classList.contains('open')
-            ? (fix_content_height.offsetHeight + 10) + 'px'
-            : 0
-    )
+    column_fix_height(container)
 }
 
 const open_column = document.querySelectorAll('.open-column')
@@ -63,3 +68,53 @@ const handle_password_eyes = (btn) => {
 }
 const show_passwords = document.querySelectorAll('.container-eyes')
 show_passwords.forEach(btn => btn.addEventListener('click', ev => handle_password_eyes(btn), false))
+
+
+const drop_images = document.querySelector('.drop-images')
+const thumbnails = document.querySelector('.thumbnails')
+
+const build_thumbnail_item = (reader, file) => {
+
+    const item = document.createElement('span')
+
+    if( file.type != "video/mp4" ) {
+
+        const img = document.createElement('img')
+        img.src = reader.result
+        item.append(img)
+    } else {
+
+        const video = document.createElement('video')
+        const source = document.createElement('source')
+        source.src = reader.result
+
+        video.append(source)
+        item.append(video)
+    }
+
+    return item
+}
+
+const handle_upload_image = () => {
+
+    const files = drop_images.files;
+
+    Array.from(files).forEach(async file => {
+
+        const reader = new FileReader()
+
+        reader.onload = () => {
+
+            thumbnails.append(
+                build_thumbnail_item(reader, file)
+            )
+
+            column_fix_height(
+                drop_images.closest('.col-content-manager')
+            )
+        }
+
+        reader.readAsDataURL(file);
+    })
+}
+drop_images.addEventListener('change', ev=>handle_upload_image(), false)
