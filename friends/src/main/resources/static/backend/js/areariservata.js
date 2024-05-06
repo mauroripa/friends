bb47bce4c9e6f5ebd8a0286b6e766ad9cec5eb9f
Andrei Leca <HMDCrew@users.noreply.github.com>
2024-05-06 15:22:18 +0200

    JSDoc Area riservata

--- friends/src/main/resources/static/backend/js/areariservata.js
+++ friends/src/main/resources/static/backend/js/areariservata.js
@@ -1,3 +1,12 @@
+
+/**
+ * The function `column_fix_height` adjusts the height of a container element based on its child
+ * elements.
+ * @param container - The `container` parameter is a reference to the parent element that contains the
+ * elements with the classes `.carta` and `.carta-content-fix`. The function `column_fix_height`
+ * calculates and sets the height of the `.carta` element based on whether the container has the class
+ * `open`
+ */
 const column_fix_height = (container) => {
 
     const carta = container.querySelector('.carta')
@@ -14,6 +23,12 @@ const column_fix_height = (container) => {
 const pre_opened_columns = document.querySelector('.col-content-manager.open')
 pre_opened_columns && column_fix_height(pre_opened_columns)
 
+/**
+ * The `handle_column` function toggles the 'open' class on a container element and scrolls the content
+ * to the top.
+ * @param btn - The `btn` parameter in the `handle_column` function is a reference to the button
+ * element that triggered the event.
+ */
 const handle_column = (btn) => {
 
     const container = btn.closest('.col-content-manager')
@@ -32,6 +47,12 @@ const close_column = document.querySelectorAll('.close-column')
 close_column.forEach(btn => btn.addEventListener('click', ev => handle_column(btn), false))
 
 
+/**
+ * The function `handle_password_eyes` toggles between displaying a password as plain text or masked
+ * with asterisks.
+ * @param btn - The `btn` parameter in the `handle_password_eyes` function is a reference to the button
+ * element that triggered the function.
+ */
 const handle_password_eyes = (btn) => {
 
     const container = btn.closest('.pwd-info')
@@ -54,10 +75,17 @@ const drop_images = document.querySelector('.drop-images')
 const drop_area = document.querySelector('.droparea')
 const thumbnails = document.querySelector('.thumbnails .thumbnails-content')
 
+/**
+ * The function `drag_image_in` adds a class of 'hover' to the drop area element.
+ */
 const drag_image_in = () => {
     drop_area.classList.add('hover')
 }
 
+/**
+ * The function `drag_image_out` removes the classes 'hover' and 'is-invalid' from the drop area
+ * element and adds the class 'droped'.
+ */
 const drag_image_out = () => {
     drop_area.classList.remove('hover')
     drop_area.classList.remove('is-invalid')
@@ -70,6 +98,20 @@ drop_area.addEventListener('dragleave', ev => drag_image_out(), false)
 drop_area.addEventListener('drop', ev => drag_image_out(), false)
 
 
+/**
+ * The function `remove_item_btn` creates a remove button with an SVG icon and functionality to remove
+ * an item from a list and update the display accordingly.
+ * @param item - The `item` parameter in the `remove_item_btn` function represents the HTML element
+ * that you want to remove when the remove button is clicked. This element is typically an image or a
+ * file preview that you want to delete from a list or display area. When the remove button is clicked,
+ * the associated
+ * @param file - The `file` parameter in the `remove_item_btn` function represents the file object that
+ * is being removed when the remove button is clicked. This file object is used to identify the
+ * specific file that needs to be removed from the list of files displayed in the user interface.
+ * @returns The `remove_item_btn` function is returning a dynamically created `<span>` element with a
+ * delete icon in SVG format. This element has a click event listener attached to it that removes the
+ * specified item from a list of files and updates the UI accordingly.
+ */
 const remove_item_btn = (item, file) => {
 
     const remove_btn = document.createElement('span')
@@ -96,6 +138,21 @@ const remove_item_btn = (item, file) => {
     return remove_btn
 }
 
+/**
+ * The function `build_thumbnail_item` creates a thumbnail element for an image or video file with a
+ * remove button.
+ * @param reader - The `reader` parameter in the `build_thumbnail_item` function is typically an
+ * instance of the `FileReader` object in JavaScript. This object allows you to read the contents of
+ * files asynchronously. In this context, it is used to read the contents of a file and generate a
+ * thumbnail item based on
+ * @param file - The `file` parameter in the `build_thumbnail_item` function represents the file that
+ * is being processed to create a thumbnail item. The function checks the type of the file to determine
+ * whether to create an image element or a video element for the thumbnail item.
+ * @returns The `build_thumbnail_item` function returns a dynamically created HTML element (either an
+ * image or a video element) based on the type of file being processed. If the file is not a video file
+ * (type is not "video/mp4"), an image element is created and returned. If the file is a video file, a
+ * video element with a source element is created and returned.
+ */
 const build_thumbnail_item = (reader, file) => {
 
     const item = document.createElement('span')
@@ -122,6 +179,10 @@ const build_thumbnail_item = (reader, file) => {
     return item
 }
 
+/**
+ * The function `handle_upload_image` reads and displays uploaded image files as thumbnails in a
+ * container.
+ */
 const handle_upload_image = () => {
 
     const files = drop_images.files;
@@ -150,6 +211,17 @@ drop_images.addEventListener('change', ev => handle_upload_image(), false)
 // required all fields before sumbit
 const submit_columns = document.querySelectorAll('.col-content-manager [type="submit"]')
 
+/**
+ * The function `check_empty_fields` checks for empty fields in a form and adds a CSS class to mark
+ * them as invalid.
+ * @param form - The `form` parameter in the `check_empty_fields` function is expected to be a
+ * reference to an HTML form element. This function is designed to check for empty fields within this
+ * form, including input fields, select elements, and textareas. It iterates over these form elements
+ * and checks if they
+ * @returns The function `check_empty_fields` returns a boolean value indicating whether there are any
+ * empty fields in the form. If there are empty fields, it returns `true`, otherwise it returns
+ * `false`.
+ */
 const check_empty_fileds = (form) => {
 
     let has_empty_fields = false
@@ -172,6 +244,17 @@ const check_empty_fileds = (form) => {
     return has_empty_fields
 }
 
+/**
+ * The function `handle_submit` checks for empty fields in a form and prevents form submission if any
+ * are found.
+ * @param ev - The `ev` parameter typically represents the event object that is passed to an event
+ * handler function in JavaScript. It contains information about the event that occurred, such as the
+ * type of event, the target element, and any additional data related to the event. In this case, it is
+ * being used in the
+ * @param form - The `form` parameter in the `handle_submit` function likely refers to the HTML form
+ * element that triggered the submit event. This parameter allows the function to access the form's
+ * data and perform validation or other actions based on the form's content.
+ */
 const handle_submit = (ev, form) => {
 
     if( check_empty_fileds(form) ) {
